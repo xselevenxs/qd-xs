8cc664cf34fc8e1f74856aef1393b32ccf301df9
xushuai <872219381@qq.com>
2020-10-28 23:16:25 +0800

    开发

---
+++ qdad-gd/pages/convert/convert.vue
@@ -0,0 +1,83 @@
+<template>
+	<view class="container">
+		<view class="header">
+			<view class="ticket">
+				<view class="type">洗车券</view>
+				<view class="from">由XXX提供洗车券一张</view>
+				<view class="time">有效期xxxx年xx月xx日</view>
+				<view class="btn">兑换</view>
+			</view>
+		</view>
+		<view class="adress">
+			<view>门店地址：</view>
+			<view>XXXX店</view>
+			<view>营业时间：8：00-16：00</view>
+			<view>使用方式</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+.header {
+		width: 100%;
+		height: 400upx;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		font-size: 30upx;
+	}
+	.ticket {
+		width: 700upx;
+		height: 370upx;
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		background-color: #D2F1F0;
+		position: relative;
+	}
+	.type {
+		position: absolute;
+		left: 40upx;
+		top: 30upx;
+		color: #DD514C;
+		font-size: 45upx;
+	}
+	.from {
+		position: absolute;
+		left: 20upx;
+		bottom: 100upx;
+		color: #999999;
+		font-size: 35upx;
+	}
+	.time {
+		position: absolute;
+		left: 20upx;
+		bottom: 20upx;
+		color: #999999;
+	}
+	.btn {
+		position: absolute;
+		right: 40upx;
+		bottom: 200upx;
+		color: #FFFFFF;
+		background-color: #39B54A;
+		padding: 20upx 40upx;
+		border-radius: 5upx;
+	}
+	.adress {
+		padding: 40upx;
+	}
+</style>
---
+++ qdad-gd/pages/submitInfo/submitInfo.vue
@@ -0,0 +1,25 @@
+<template>
+	<view>
+		<image src="../../static/car.jpeg" class="img" ></image>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+.img{
+	width: 100%;
+	height: 400upx;
+}
+</style>
